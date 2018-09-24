74e068700e9cb79f6ba03ab5fcfe132133982fe3
Elias David Lara Guzmán <42329259+badrobobot@users.noreply.github.com>
2018-09-24 17:37:27 -0500

    Update cidg_p4.py

--- cidg_p4.py
+++ cidg_p4.py
@@ -1,2 +1,14 @@
 while True:
+  while True:
+    deg = input("Write a sexagecimal degree: ")
+    try:
+      deg = int(deg)
+      break
+    except ValueError:
+      print("Invalid charecter. Try again")
+      pass 
+    
+  print(deg)
   pass
+      
+ 
